340e6a56fcda8680c72566d44ff285c31f42ed1a
TomG <tomgegs@outlook.com>
2022-12-01 19:33:29 +1000

    Revision of all design and content. Starting over with cleaner, simpler and better articulate approach

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // function to transition nav bar from transparent to colored on scroll
 
 function changeBg() {
-    var navBarJs = document.getElementById('navBarJs')
-    var scrollValue = window.scrollY;
+    let navBarJs = document.getElementById('navBarJs')
+    let scrollValue = window.scrollY;
     if(scrollValue < 100){
         navBarJs.classList.remove('navBarTransparentBackground')
 }   else {
@@ -12,4 +12,29 @@ function changeBg() {
 
 window.addEventListener('scroll', changeBg)
 
-//
+//function to reveal and hide about me text 
+
+let shortAboutMeText = document.getElementById("shortAboutMeText");
+let longAboutMeText = document.getElementById("longAboutMeText");
+let aboutMeButton = document.getElementById("aboutMeToggle")
+  
+
+// function toggleShortLongAboutMeText() {
+//     if (longAboutMeText.style.display === 'none' && shortAboutMeText.style.display === 'block') 
+//     {
+//         longAboutMeText.style.display = "block" && shortAboutMeText.style.display === 'none'
+//     } else {
+//         longAboutMeText.style.display = "none" && longAboutMeText.style.display === 'block'
+//     }
+
+
+// }
+
+aboutMeToggle.addEventListener('click', () => {
+    if (longAboutMeText.style.display === 'none' && shortAboutMeText.style.display === 'block') {    
+        longAboutMeText.style.display = "block" && shortAboutMeText.style.display === 'none'
+    } else {
+        longAboutMeText.style.display = "none" && longAboutMeText.style.display === 'block'
+    }
+}
+)
